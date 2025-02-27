253bf3f38144dbba4918a71915b1f6a880bb8306
dhadner <93440039+dhadner@users.noreply.github.com>
2025-02-27 12:20:59 -0800

    Fix annoying messages in IDE

--- 68000EmulatorLib/OpcodeExecutionHandler.cs
+++ 68000EmulatorLib/OpcodeExecutionHandler.cs
@@ -17,14 +17,14 @@ namespace PendleCodeMonkey.MC68000EmulatorLib
         {
             delegate TrapException? OpHandler(Instruction instruction);
 
-            private Dictionary<OpHandlerID, OpHandler> _handlers = new Dictionary<OpHandlerID, OpHandler>();
+            private readonly Dictionary<OpHandlerID, OpHandler> _handlers = [];
 
-            private readonly uint[] _bit = new uint[] { 0x00000001, 0x00000002, 0x00000004, 0x00000008, 0x00000010, 0x00000020, 0x00000040, 0x00000080,
-                                                        0x00000100, 0x00000200, 0x00000400, 0x00000800, 0x00001000, 0x00002000, 0x00004000, 0x00008000,
-                                                        0x00010000, 0x00020000, 0x00040000, 0x00080000, 0x00100000, 0x00200000, 0x00400000, 0x00800000,
-                                                        0x01000000, 0x02000000, 0x04000000, 0x08000000, 0x10000000, 0x20000000, 0x40000000, 0x80000000 };
+            private readonly uint[] _bit = [ 0x00000001, 0x00000002, 0x00000004, 0x00000008, 0x00000010, 0x00000020, 0x00000040, 0x00000080,
+                                             0x00000100, 0x00000200, 0x00000400, 0x00000800, 0x00001000, 0x00002000, 0x00004000, 0x00008000,
+                                             0x00010000, 0x00020000, 0x00040000, 0x00080000, 0x00100000, 0x00200000, 0x00400000, 0x00800000,
+                                             0x01000000, 0x02000000, 0x04000000, 0x08000000, 0x10000000, 0x20000000, 0x40000000, 0x80000000 ];
 
-            private readonly object lockObj = new object();     // Object that is locked for TAS instruction.
+            private readonly object lockObj = new();     // Object that is locked for TAS instruction.
 
             internal int _numberOfJSRCalls = 0;
 
