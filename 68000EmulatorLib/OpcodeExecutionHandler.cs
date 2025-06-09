8ad88ee53a786bcfea7831c4d2b03ab518042517
dhadner <93440039+dhadner@users.noreply.github.com>
2025-06-09 09:03:30 -0700

    Use Rust-based lock.  Appears to have a slight performance hit, but hopefully we'll make up for it later by running the Machine in Rust.

--- 68000EmulatorLib/OpcodeExecutionHandler.cs
+++ 68000EmulatorLib/OpcodeExecutionHandler.cs
@@ -2173,7 +2173,7 @@ namespace PendleCodeMonkey.MC68000EmulatorLib
 
             private TrapException? TAS(Instruction inst)
             {
-                lock (PendleCodeMonkey.MC68000EmulatorLib.Machine.Lock)
+                using (PendleCodeMonkey.MC68000EmulatorLib.Lock.Acquire())
                 {
                     var value = ReadEAValue(inst, EAType.Destination, suppressIncDec: true);
                     if (value.HasValue)
