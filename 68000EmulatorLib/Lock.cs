2229a2e08f73bd0265cfa88fb8eb9707391754ab
dhadner <93440039+dhadner@users.noreply.github.com>
2025-06-09 12:16:43 -0700

    Debugger callbacks for machine.rs.  Not tested yet.

--- 68000EmulatorLib/Lock.cs
+++ 68000EmulatorLib/Lock.cs
@@ -6,6 +6,16 @@ namespace PendleCodeMonkey.MC68000EmulatorLib
     /// <summary>
     /// Acquire/release a lock.  This is a global lock for the emulator.  Using any
     /// additional lock methods should be carefully reviewed to avoid deadlocks.
+    /// 
+    /// The main use case for this lock is to control access to resources shared
+    /// between the UI thread (debugger, disassembler, etc.) and the emulator machine 
+    /// execution thread.
+    /// 
+    /// Because of this simplicity (e.g., only two threads in general), using a 
+    /// simple one-lock mechanism should be sufficient.  If additional threads or resources
+    /// should be needed in the future, this lock could be enhanced to support 
+    /// multiple locks with different levels to ensure that multiple locks can be acquired
+    /// in only in the canonical order, thus preventing deadlocks.
     /// </summary>
     public static class Lock
     {
