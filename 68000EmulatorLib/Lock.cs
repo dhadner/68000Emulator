209309b0a28a78e4e033ed75cad663287be39fef
dhadner <93440039+dhadner@users.noreply.github.com>
2025-06-09 08:45:43 -0700

    WIP - using Rust locking - prior.

---
+++ 68000EmulatorLib/Lock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PendleCodeMonkey.MC68000EmulatorLib
+{
+    /// <summary>
+    /// Acquire/release a lock.  This is a global lock for the emulator.  Using any
+    /// additional lock methods should be carefully reviewed to avoid deadlocks.
+    /// </summary>
+    public static class Lock
+    {
+        [DllImport("macse_rust.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int lock_acquire();
+
+        [DllImport("macse_rust.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int lock_release();
+
+        public static IDisposable Acquire()
+        {
+            if (lock_acquire() != 0)
+            {
+                throw new InvalidOperationException("Failed to acquire Rust lock");
+            }
+            return new Releaser();
+        }
+
+        private sealed class Releaser : IDisposable
+        {
+            private bool _disposed = false;
+            public void Dispose()
+            {
+                if (!_disposed)
+                {
+                    if (lock_release() != 0)
+                    {
+                        const string errorMessage = "Failed to release Rust lock - Attempted to release lock from a different thread";
+                        Console.WriteLine(errorMessage);
+                        throw new InvalidOperationException(errorMessage);
+                    }
+                    _disposed = true;
+                }
+            }
+        }
+    }
+}
