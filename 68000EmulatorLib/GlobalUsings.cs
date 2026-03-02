adba40d18a0914160da39614b097fd6af341b03e
dhadner <93440039+dhadner@users.noreply.github.com>
2026-03-01 16:37:49 -0800

    Remove unnecessary global Result<T,TError> usings since Result<TError> has an Ok(T) method that returns a Result<T,TError> with IsSuccess true.

--- 68000EmulatorLib/GlobalUsings.cs
+++ 68000EmulatorLib/GlobalUsings.cs
@@ -4,10 +4,6 @@
 // .NET version for the code: .NET 10.0
 // Global usings for the MC68000EmulatorLib project
 global using Stardust.Utilities;
-global using static Stardust.Utilities.Result<byte, string>;
-global using static Stardust.Utilities.Result<ushort, string>;
-global using static Stardust.Utilities.Result<uint, string>;
-global using static Stardust.Utilities.Result<byte[], string>;
 global using static Stardust.Utilities.Result<string>;
 
 
